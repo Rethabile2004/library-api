f563364d5dab3164f773aa1ad179ec5528b30bbd
Rethabile Siase <ericksiase375@gmail.com>
2026-06-02 16:58:11 +0200

    feat: finalize Library API with borrow system, auth, logging, and cleanup

--- LibraryApi/Controllers/AuthController.cs
+++ LibraryApi/Controllers/AuthController.cs
@@ -15,11 +15,13 @@ namespace LibraryApi.Controllers
         private readonly AppDbContext _context;
         private readonly IConfiguration _configuration;
         private readonly ITokenService _tokenService;
-        public AuthController(AppDbContext context, IConfiguration configuration, ITokenService tokenService)
+        private readonly ILogger<AuthController> _logger;
+        public AuthController(AppDbContext context, IConfiguration configuration, ITokenService tokenService, ILogger<AuthController> logger)
         {
             _context = context;
             _configuration = configuration;
             _tokenService = tokenService;
+            _logger = logger;
         }
         //api/auth/register
         [HttpPost("register")]
@@ -29,6 +31,7 @@ namespace LibraryApi.Controllers
             var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == registerDto.Email.ToLower());
             if (existingUser != null)
             {
+                _logger.LogWarning("Registration attempt with already existing email: {Email}", registerDto.Email);
                 // return conflict 409 if the account already exists
                 return Conflict(new { message = "A user with this email already exists." });
             }
@@ -43,8 +46,10 @@ namespace LibraryApi.Controllers
             await _context.Users.AddAsync(newUser);
             await _context.SaveChangesAsync();
 
+            _logger.LogInformation("New user registered Email: {Email} (UserId: {UserId})", newUser.Email, newUser.Id);
             var token = _tokenService.GenerateToken(newUser);
             var expiryTime = int.Parse(_configuration["JwtSettings:ExpiryHours"]!);
+
             return Ok(new AuthResponseDto
             {
                 Email = newUser.Email,
@@ -61,16 +66,20 @@ namespace LibraryApi.Controllers
             var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == loginDto.Email.ToLower());
             if (user == null)
             {
+                _logger.LogWarning("Failed login attempt email: {Email} not found.", loginDto.Email);
                 // 401 credentials are missing or invalid
                 return Unauthorized(new { message = "Invalid email or password." });
             }
-            var validPassword = BCrypt.Net.BCrypt.Verify(loginDto.Password,user.PasswordHash);
+            var validPassword = BCrypt.Net.BCrypt.Verify(loginDto.Password, user.PasswordHash);
             if (!validPassword)
             {
+                _logger.LogWarning("Failed login attempt incorrect password for Email: {Email}", loginDto.Email);
                 return Unauthorized(new { message = "Invalid email or password." });
             }
+
             var token = _tokenService.GenerateToken(user);
             var expiryTime = int.Parse(_configuration["JwtSettings:ExpiryHours"]!);
+            _logger.LogInformation("User logged in: {Email} (UserId: {UserId})", user.Email, user.Id);
             return Ok(new AuthResponseDto
             {
                 Email = user.Email,
