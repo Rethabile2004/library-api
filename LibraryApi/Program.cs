5b005bce414ec813c0276eb0557cbba3b0fea45d
Rethabile Siase <ericksiase375@gmail.com>
2026-06-10 21:40:42 +0200

    fix for deployment

--- LibraryApi/Program.cs
+++ LibraryApi/Program.cs
@@ -148,7 +148,8 @@ try
     {
         var uri = new Uri(rawConn);
         var userInfo = uri.UserInfo.Split(':');
-        connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+        var localPort = uri.Port == -1 ? 5432 : uri.Port;
+        connectionString = $"Host={uri.Host};Port={localPort};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
     }
     else
     {
